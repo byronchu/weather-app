ea18bfa7a766453b028da43dd79d3ddf1b50e172
unknown <chu.byron@yahoo.com>
2017-04-25 22:32:02 +1000

    added error handling to Callback to google API server

--- app.js
+++ app.js
@@ -21,8 +21,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  if (error) {
+    console.log('Unable to connect to Google Servers')
+  } else if (body.status === 'ZERO_RESULTS' ) {
+    console.log('Address not found')
+  } else if (body.status === 'OK') {
+//  console.log(JSON.stringify(body, undefined, 2));
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
